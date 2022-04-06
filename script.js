f48323f36e8600b8b14fecdcb86b5cdc9609eedc
carlo-ddm <carloduminuco90@gmail.com>
2022-04-07 01:41:07 +0200

    Migliore logica di lettura in console / aggiuta di commento /

--- script.js
+++ script.js
@@ -11,34 +11,34 @@ console.log ('Prezzo del viaggio al chilometro',price_per_kil);
 
 // prezzo senza scontistica
 const price = price_per_kil * kil;
-console.log ('Prezzo del viaggio senza scontistica',price);
+// const message = 'Non hai diritto alla scontistica'--> Per perfezionare l'esercizio mettendo in stampa la lista dei calcoli e l'importo al centro
 
 if (age > 18 && age < 65) {
+  console.log ('Prezzo del viaggio senza scontistica',price);
   document.getElementById ("prezzo").innerHTML += " "+price.toFixed(2)+" ";
 }
 
 // scontistica under 18
 const discountUnderage = ((price * 20) / 100);
-console.log ('Sconto del 20% sul prezzo', discountUnderage);
-
 const discountUnder_price = price - discountUnderage;
-console.log ('Prezzo scontato under 18', discountUnder_price);
 
 if (age <= 18){
-  discountUnder_price;
+  console.log ('Prezzo del viaggio senza scontistica',price);
+  console.log ('Sconto del 20% sul prezzo', discountUnderage);
+  console.log ('Prezzo scontato under 18', discountUnder_price);
   document.getElementById ("prezzo").innerHTML +=  " "+discountUnder_price.toFixed(2)+" ";
 } 
 
 // scontistica over 65
 const discountOver = ((price * 40) / 100);
-console.log ('Sconto del 40% sul prezzo', discountOver);
-
 const discountOver_price = price - discountOver;
-console.log ('Prezzo scontato over 65', discountOver_price);
 
 if (age >= 65){
-  discountOver_price;
+  console.log ('Prezzo del viaggio senza scontistica',price);
+  console.log ('Sconto del 40% sul prezzo', discountOver);
+  console.log ('Prezzo scontato over 65', discountOver_price);
   document.getElementById ("prezzo").innerHTML += " "+discountOver_price.toFixed(2)+ " ";
 }
 
 // Gli "else" in questo caso sono solo in aggiunta
+
